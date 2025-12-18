57e38a90e3477636d3f90b6e797464ca91b88e47
bbingsuu <cavide.kurtes@ogr.dpu.edu.tr>
2025-12-19 01:36:27 +0300

    feat: add chart widget implementation with initial setup for data visualization

---
+++ app/views/chart_widget.py
@@ -0,0 +1,5 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from datetime import datetime
